ffeeac0bea3e56c52cf02bf1bcd868a28095fc20
John Rampono <john.rampono@insight.com>
2025-06-30 18:36:46 +0800

    Cleaning up

--- .devcontainer/spark_minimal/spark.dockerfile
+++ .devcontainer/spark_minimal/spark.dockerfile
@@ -1,17 +1,6 @@
 # Base image with Python 3.11 on Debian Bookworm
 FROM bitnami/spark:4.0-debian-12
 
-
-
-
-# (Optional) Install common PySpark dependencies
-RUN pip install pyspark==4.0.0
-RUN pip install delta-spark==4.0.0
-RUN pip install jupyterlab 
-RUN pip install pandas 
-RUN pip install matplotlib 
-
-
 # Set environment variables
 ENV SPARK_HOME=/opt/bitnami/spark
 ENV PATH="$SPARK_HOME/bin:$PATH"
---
+++ scripts/dev_container_scripts/spark_minimal/dev_tools.ps1
@@ -0,0 +1,36 @@
+# Install zip
+apt-get install curl
+apt-get install zip
+
+
+# Install Oh My Posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+# Add /bin to the PATH environment variable
+$env:PATH += ";/bin"
+
+oh-my-posh font install CascadiaCode 
+
+# Suggest setting font to Cascadia Code NFM in vs code settings
+# "terminal.integrated.fontFamily": "CaskaydiaCove Nerd Font, monospace",
+
+Install-Module -Name Terminal-Icons -Scope CurrentUser -Force
+Install-Module -Name posh-git -Scope CurrentUser -Force
+
+# Install Git 
+apt install git -y
+
+#### GH CLI for Authentication with GitHub ####
+bash ./scripts/dev_container_scripts/spark_minimal/git_cli_install.sh 
+
+# Copy sample profile to home directory
+if (-not (Test-Path -Path $PROFILE)) {
+    New-Item -ItemType File -Path $PROFILE -Force
+}
+cp ./scripts/dev_container_scripts/spark_minimal/pwsh_profile_sample.txt $PROFILE
+
+# Pip Installs 
+pip install uv
+uv venv
+./.venv/bin/activate.ps1
+uv sync
