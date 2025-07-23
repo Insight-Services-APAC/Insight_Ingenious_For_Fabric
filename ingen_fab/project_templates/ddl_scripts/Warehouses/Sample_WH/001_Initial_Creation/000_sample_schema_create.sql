27a8f96d38242152cc19e484b1643bd9b2d9ac0a
John Rampono <john.rampono@insight.com>
2025-07-24 05:49:21 +0800

    Fixing warehouse ddl bugs

---
+++ ingen_fab/project_templates/ddl_scripts/Warehouses/Sample_WH/001_Initial_Creation/000_sample_schema_create.sql
@@ -0,0 +1,6 @@
+-- Sample DDL script for creating a customer table in warehouse
+-- This demonstrates the basic pattern for creating T-SQL tables
+
+-- Create the sample schema
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'config')
+    exec('CREATE SCHEMA config;')
